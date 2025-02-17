e51e6854952c1d96c1dc86e04e7aeb372ad93bd1
Bermelio <facundoperalta258@gmail.com>
2025-02-17 17:23:42 -0300

    logica funcionando falta consumirla

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -4,11 +4,26 @@ socket.emit('message', 'Hello World!');
 
 document.getElementById('formMain').addEventListener('submit', (e) => {
     e.preventDefault();
+});
 
-    const nameProduct = document.getElementById('productName').value;
-    const price = document.getElementById('price').value;
+const btnSend = document.getElementById('send');
 
-    const newProduct = {nameProduct,price};
+btnSend.addEventListener('click', () => {
+    const inputProductName = document.getElementById('productName');
+    const inputPrice = document.getElementById('price');
+
+    const productName = inputProductName.value;
+    const price = inputPrice.value;
+    
+    console.log("🛒 Producto ingresado:", productName, "💰 Precio:", price);
+
+    inputProductName.value = '';
+    inputPrice.value = '';
+
+
+    const newProduct = { name: productName, price };
+
+    console.log("📦 Objeto enviado al servidor:", newProduct);
 
     socket.emit('newProduct', newProduct);
 });
